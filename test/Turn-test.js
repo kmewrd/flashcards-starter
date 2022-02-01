4ba02fd80bec579d3ee01532f2b43889dddb5f07
Kim Ward <kmewrd@outlook.com>
2022-02-01 14:38:07 -0600

    Link Card file and add test that expects Turn to take in the current card

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+const Card = require('../src/Card');
 
 describe('Turn', function() {
 
@@ -15,5 +16,13 @@ describe('Turn', function() {
     const turn = new Turn();
 
     expect(turn).to.be.an.instanceOf(Turn);
-  })
+  });
+
+  it('should be able to take in the current card in play', function() {
+    const card = new Card(1, 'What is Kim\'s favorite animal?', ['penguin', 'armadillo', 'stoat', 'tardigrade'], 'stoat');
+    const turn = new Turn(card);
+
+    expect(turn.currentCard).to.be.an.instanceOf(Card);
+    expect(turn.currentCard.correctAnswer).to.equal('stoat');
+  });
 });
