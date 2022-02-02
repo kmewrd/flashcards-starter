29c02f1050e1e20e9267f092d59f6107f53016b4
Kim Ward <kmewrd@outlook.com>
2022-02-01 20:36:36 -0600

    Add test that checks if the Deck has stored the number of Cards

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -27,4 +27,14 @@ describe('Deck', function() {
 
     expect(deck.cards).to.equal([card1, card2, card3]);
   });
+
+  it('should know how many Cards are in the Deck', function() {
+    const card1 = new Card(1, 'Which of the following is an array iterator method?', ['push()', 'concat()', 'reduce()', 'splice()'], 'reduce()');
+    const card2 = new Card(2, 'What is the minimum number of arguments that a reducer callback function requires?', [1, 3, 2, 4], 2);
+    const card3 = new Card(3, 'What does the map() iterator method return?', ['an array that is a different length from the calling array', 'an array that is the same length as the calling array', 'a single value that results from running a callback function over the array', 'nothing'], 'an array that is the same length as the calling array');
+
+    const deck = new Deck([card1, card2, card3]);
+
+    expect(deck.numberOfCards).to.equal(3);
+  });
 });
