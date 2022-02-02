b9f262ced4878f2a08d8af5aff43995ebb95e113
Kim Ward <kmewrd@outlook.com>
2022-02-01 21:03:25 -0600

    Add test that checks for the current card

--- test/Round-test.js
+++ test/Round-test.js
@@ -32,4 +32,17 @@ describe('Round', function() {
 
     expect(round).to.be.an.instanceOf(Round);
   });
+
+  it('should keep track of the current card', function() {
+    const card1 = new Card(1, 'Which of the following is an array iterator method?', ['push()', 'concat()', 'reduce()', 'splice()'], 'reduce()');
+    const card2 = new Card(2, 'What is the minimum number of arguments that a reducer callback function requires?', [1, 3, 2, 4], 2);
+    const card3 = new Card(3, 'What does the map() iterator method return?', ['an array that is a different length from the calling array', 'an array that is the same length as the calling array', 'a single value that results from running a callback function over the array', 'nothing'], 'an array that is the same length as the calling array');
+
+    const allCards = [card1, card2, card3];
+
+    const deck = new Deck(allCards);
+    const round = new Round(deck);
+
+    expect(deck.currentCard).to.equal(card1);
+  });
 });
