dfa8ca071fb323af5ba22a564fda92bac14a60ff
Kim Ward <kmewrd@outlook.com>
2022-02-01 20:52:23 -0600

    Remove default properties

--- src/Deck.js
+++ src/Deck.js
@@ -1,7 +1,7 @@
 class Deck {
   constructor(cards) {
-    this.cards = cards || [];
-    this.numberOfCards = this.cards.length || 0;
+    this.cards = cards;
+    this.numberOfCards = this.cards.length;
   }
 }
 
