c3b19afd8f216bd6faef8d84bb4bc67170cdd52f
Kim Ward <kmewrd@outlook.com>
2022-02-01 20:37:54 -0600

    Fix typo in first it block

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -9,7 +9,7 @@ describe('Deck', function() {
   it('should be a function', function() {
     const deck = new Deck();
 
-    expect(deck).to.be.a('function');
+    expect(Deck).to.be.a('function');
   });
 
   it('should be an instance of Deck', function() {
