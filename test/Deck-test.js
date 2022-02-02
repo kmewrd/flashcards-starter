19ea68c0f9d073ceeb65f7590c151cdba7a600c8
Kim Ward <kmewrd@outlook.com>
2022-02-01 20:25:30 -0600

    Add describe block and initial tests to check for class

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -3,3 +3,18 @@ const expect = chai.expect;
 
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
+
+describe('Deck', function() {
+
+  it('should be a function', function() {
+    const deck = new Deck();
+
+    expect(deck).to.be.a('function');
+  });
+
+  it('should be an instance of Deck', function() {
+    const deck = new Deck();
+
+    expect(deck).to.be.an.instanceOf(Deck);
+  });
+});
