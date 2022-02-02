6494aeff8c2cf124a0731a3ba17f8851f9427638
Kim Ward <kmewrd@outlook.com>
2022-02-02 15:36:33 -0600

    Add test that checks if currentCard is changing

--- test/Round-test.js
+++ test/Round-test.js
@@ -98,6 +98,22 @@ describe('Round', function() {
     expect(round.incorrectGuesses).to.equal([]);
   });
 
+  it('should automatically change cards after each guess', function() {
+    const card1 = new Card(1, 'Which of the following is an array iterator method?', ['push()', 'concat()', 'reduce()', 'splice()'], 'reduce()');
+    const card2 = new Card(2, 'What is the minimum number of arguments that a reducer callback function requires?', [1, 3, 2, 4], 2);
+    const card3 = new Card(3, 'What does the map() iterator method return?', ['an array that is a different length from the calling array', 'an array that is the same length as the calling array', 'a single value that results from running a callback function over the array', 'nothing'], 'an array that is the same length as the calling array');
+
+    const allCards = [card1, card2, card3];
+
+    const deck = new Deck(allCards);
+    const round = new Round(deck);
+
+    round.takeTurn('reduce()');
+
+    expect(round.currentCard).to.equal(card2);
+    expect(round.returnCurrentCard()).to.equal(card2);
+  });
+
   it('should increment turns with each guess', function() {
     const card1 = new Card(1, 'Which of the following is an array iterator method?', ['push()', 'concat()', 'reduce()', 'splice()'], 'reduce()');
     const card2 = new Card(2, 'What is the minimum number of arguments that a reducer callback function requires?', [1, 3, 2, 4], 2);
