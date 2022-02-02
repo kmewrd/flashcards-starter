d00de271f0e57f93f225146c6a7784c030590a0b
Kim Ward <kmewrd@outlook.com>
2022-02-01 20:41:22 -0600

    Store cards array in a variable and modify test to pass in variable

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -23,9 +23,11 @@ describe('Deck', function() {
     const card2 = new Card(2, 'What is the minimum number of arguments that a reducer callback function requires?', [1, 3, 2, 4], 2);
     const card3 = new Card(3, 'What does the map() iterator method return?', ['an array that is a different length from the calling array', 'an array that is the same length as the calling array', 'a single value that results from running a callback function over the array', 'nothing'], 'an array that is the same length as the calling array');
 
-    const deck = new Deck([card1, card2, card3]);
+    const allCards = [card1, card2, card3];
 
-    expect(deck.cards).to.equal([card1, card2, card3]);
+    const deck = new Deck(allCards);
+
+    expect(deck.cards).to.equal(allCards);
   });
 
   it('should know how many Cards are in the Deck', function() {
