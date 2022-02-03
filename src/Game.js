b6ccd960ea259dcf52810ae9956769686f2342bd
Kim Ward <kmewrd@outlook.com>
2022-02-02 21:33:13 -0600

    Invoke printMessage and printQuestion inside of start method

--- src/Game.js
+++ src/Game.js
@@ -22,6 +22,8 @@ class Game {
     this.cards = prototypeQuestions.map(card => new Card(card.id, card.question, card.answers, card.correctAnswer));
     this.deck = new Deck(this.cards);
     this.currentRound = new Round(this.deck);
+    this.printMessage(this.deck, this.currentRound);
+    this.printQuestion(this.currentRound);
   }
 }
 
