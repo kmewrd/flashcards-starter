16b21c55ff6837821b1109a4dd1ac39f49465ab6
Kim Ward <kmewrd@outlook.com>
2022-02-03 15:58:30 -0600

    Change return to console.log so it prints the endRound message

--- src/Round.js
+++ src/Round.js
@@ -30,7 +30,7 @@ class Round {
     return ((this.turns - this.incorrectGuesses.length)/(this.turns)) * 100;
   }
   endRound() {
-    return `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`;
+    console.log(`** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`);
   }
 }
 
