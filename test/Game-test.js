7815bcc279ab93a44350250fa9d70857ec87b18a
Kim Ward <kmewrd@outlook.com>
2022-02-02 20:50:16 -0600

    Link chai, expect API, and files and create describe block

---
+++ test/Game-test.js
@@ -0,0 +1,14 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Turn = require('../src/Turn');
+const Round = require('../src/Round');
+const Game = require('../src/Game');
+
+describe('Game', function() {
+
+
+
+});
