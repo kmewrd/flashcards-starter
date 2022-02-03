c82f55809cad5b9b67da3efe564a07cf0b5ec583
Kim Ward <kmewrd@outlook.com>
2022-02-02 18:19:56 -0600

    Change order of operations inside takeTurn method

--- src/Round.js
+++ src/Round.js
@@ -13,14 +13,14 @@ class Round {
   takeTurn(guess) {
     this.turn = new Turn(this.currentCard, guess);
     this.turns++;
+    if (guess !== this.currentCard.correctAnswer) {
+      this.incorrectGuesses.push(this.currentCard.id);
+    }
     for (var i = 0; i < this.deck.length; i++) {
       if (this.currentCard.id === i + 1) {
         return this.currentCard = this.deck[i + 1];
       }
     };
-    if (guess !== this.currentCard.correctAnswer) {
-      this.incorrectGuesses.push(this.currentCard.id);
-    }
   }
 }
 
